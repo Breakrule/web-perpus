e30f076c86c7beacfd02400b40b2c986415f6cdb
Rezky Maulana <rezkymaulanagt@gmail.com>
2018-01-21 14:43:30 +0700

    perbaikan 2

--- index.php
+++ index.php
@@ -40,7 +40,9 @@
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Kategori <span class="caret"></span></a>
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="#">Action</a></li>
+                                <li class="divider"></li>
                                 <li><a href="#">Another action</a></li>
+                                <li class="divider"></li>
                                 <li><a href="#">Something else here</a></li>
                                 <li class="divider"></li>
                                 <li><a href="#">Separated link</a></li>
